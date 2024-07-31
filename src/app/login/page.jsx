9f2c7502546f1141c45dd001cfd10a2a6007a129
mohdakmal123 <akmal.ahmad1209@gmail.com>
2024-07-31 20:37:41 +0530

    eventhandling

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import classes from './login.module.css';
+import Input from '@/components/Input';
+import Button from '@/components/Button';
 const Login = () => {
   return (
     <div>
         <h1 className='text-center text-2xl font-bold'>Login Page</h1>
+        <button className='btn text-center'>My Button</button>
+        <button className={classes.myBtn}>Module CSS</button>
+        <Input id={'name'} label={'Full Name'} type={'text'}/>
+        <Input id={'name'} label={'Email address'} type={'email'}/>
+        <Input id={'name'} label={'Password'} type={'password'}/>
+        
+
+        <Button>Login</Button>
     </div>
   )
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,42 @@
 import React from 'react'
+import image from './next.svg';
 
 const Home  = () => {
+
+  const cartItems = 10;
+
+  const displayMessage = (msg) => {
+    return (
+      <div className='border border-red-500 bg-red-100 p-3 rounded-md w-1/2 mx-auto my-3'>
+        <span className='text-red-500 font-bold'>{msg}</span>
+      </div>
+    )
+  }
+
   return (
     <>
+    
     <div>
+      {displayMessage('oops! Your details are not correct')}
+
       <h1 style={{color:'blue', textAlign:' center',frontsize:50}}> 
         My Home Page
         </h1>
+
         <br />
+
        < input type="text"/>
+
        <img src=""alt=""/>
+
+       <h2 className='text-2xl text-center'>Cart Items:{cartItems + 5}</h2>
+       <button className='btn'>MY Button</button>
+
+       <img src="/next.svg" alt="" /> 
+       <img src={ image.src } alt="" />
+         </div>
+         <div>
+
          </div>
          
          </>
